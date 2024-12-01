5f87a665feae2401afc01d839416c0e205b10167
ist1110148 <maria.leonor.morujao@tecnico.ulisboa.pt>
2024-12-01 19:32:40 +0000

    versao antes mudar storage

--- proj.cpp
+++ proj.cpp
@@ -34,10 +34,9 @@ bool calculate(int N, int M, int target, vector<vector<vector<int>>> &tableCalcu
                 int count1 = 0; 
                 int count2 = 0;
                 for (int val1 : tableCalculated[i][k-1]) {
-                    if (count1 == 0) {
+                    if (count1 % 2 == 0) {    // Verifica se o val1 é um valor calculado ou um k
                         for (int val2 : tableCalculated[k][f]) {
-                            // Verifica se o val1 é um valor calculado ou um k
-                            if (count2 % 2 == 0) {
+                            if (count2 % 2 == 0) {    
                                 int res = tableRef[val1 - 1][val2 - 1];
 
                                 if (find(tableCalculated[i][f].begin(), tableCalculated[i][f].end(), res) == tableCalculated[i][f].end()) { //Verifica se o res ja existe
@@ -59,8 +58,20 @@ bool calculate(int N, int M, int target, vector<vector<vector<int>>> &tableCalcu
     return false;
 }
 
+
 /*
-v
+string printResult(int i, int j, int k, vector<vector<vector<int>>> &tableCalculated, int val) {
+    if (i == j)
+        return to_string(tableCalculated[i][j][0]);
+    else if (i == (j - 1))
+        return "(" + printResult(i, i, 0, tableCalculated)  + " " + printResult(j, j, 0, tableCalculated) + ")";
+    else {
+        for (int indice = 0;  : tableCalculated[i][j])
+
+
+        return "(" + printResult(i, k-1, novo_k, tableCalculated) + " )" + printResult(k, j, novo_k2, tableCalculated);
+    }
+}
 */
 
 
@@ -105,107 +116,4 @@ int main() {
  [[2, 0], [2, 1], [2, 2]],
  [[3, 0], [3, 1]],
  [[3, 0]]]
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    for (int d = 1; d < M; d++) {
-        for (int i = 0; i <= (M - d); i++) {
-            int j = i + 1;
-            if (d == 1) 
-                tableCalculated[i][j][0] = tableRef[tableCalculated[i][i][0] - 1][tableCalculated[j][j][0] - 1];
-            else {
-                int pos = 0;
-                for (int k = d - 1; k >= i; k--) {
-                    
-                    
-                }
-            }
-        }
-    }
-
-    return 0;
-}
-*/
-
-/*
-
-#include <iostream>
-#include <vector>
-#include <set>
-#include <string>
-#include <unordered_map>
-
-using namespace std;
-
-// Função para construir a parentização a partir da matriz de pares
-string constructParentization(const vector<vector<pair<int, int>>>& parent, int i, int j) {
-    if (i == j) return to_string(i + 1); // Elemento único
-    int k = parent[i][j].first;
-    return "(" + constructParentization(parent, i, k) + constructParentization(parent, k + 1, j) + ")";
-}
-
-int main() {
-    int n, m;
-    cin >> n >> m;
-
-    // Leitura da tabela de operações ⊕
-    vector<vector<int>> table(n, vector<int>(n));
-    for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            cin >> table[i][j];
-
-    // Leitura da sequência
-    vector<int> sequence(m);
-    for (int i = 0; i < m; ++i) cin >> sequence[i];
-
-    // Leitura do valor desejado
-    int target;
-    cin >> target;
-
-    // DP e matriz de parentização
-    vector<vector<set<int>>> dp(m, vector<set<int>>(m));
-    vector<vector<pair<int, int>>> parent(m, vector<pair<int, int>>(m));
-
-    // Casos base
-    for (int i = 0; i < m; ++i) dp[i][i].insert(sequence[i]);
-
-    // Preenchimento da tabela DP
-    for (int len = 2; len <= m; ++len) {
-        for (int i = 0; i + len - 1 < m; ++i) {
-            int j = i + len - 1;
-            for (int k = i; k < j; ++k) {
-                for (int a : dp[i][k]) {
-                    for (int b : dp[k + 1][j]) {
-                        int result = table[a - 1][b - 1];
-                        dp[i][j].insert(result);
-                        if (dp[i][j].count(target)) {
-                            parent[i][j] = {k, j};
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    // Verificação do valor desejado
-    if (dp[0][m - 1].count(target)) {
-        cout << "1\n";
-        cout << constructParentization(parent, 0, m - 1) << endl;
-    } else {
-        cout << "0\n";
-    }
-
-    return 0;
-}
 */
